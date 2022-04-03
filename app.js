66054824e34d549ff53d9f4a543ad107166756bc
mehedi0022 <mdmehedihassan0022@gmail.com>
2022-04-04 00:24:48 +0600

    Gussing Game

---
+++ app.js
@@ -0,0 +1,18 @@
+let wonecounter = 0;
+let lostcounter = 0;
+
+for( let i = 1; i < 6; i++){
+    let num = parseInt(prompt("Enter your Gussing Number (1 - 5): "));
+    let gussNumber = Math.random() * 5 + 1;
+    gussNumber = Math.floor(gussNumber);
+    if(num==gussNumber){
+        console.log("Yeah! You have Wone. And The Rendom Number Was " + gussNumber);
+        wonecounter++
+    }
+    else{
+        console.log("Oh! You have Lost. And The Rendom Number Was " + gussNumber);
+        lostcounter++
+    }
+}
+document.write("You have Wone:"+ " "+ wonecounter + " Times <br/>");
+document.write("You have Lost:"+ " "+ lostcounter + " Times");
